d866da04df861bb8ec19ee751242ce59ffb9815e
vikky singh <58542941+vikky-10@users.noreply.github.com>
2021-04-06 22:55:45 +0530

    fix chatinput

--- src/App.js
+++ src/App.js
@@ -54,9 +54,8 @@ function App() {
 
 export default App;
 const Container = styled.div`
-  width: 100%;
+ width: 100%;
   height: 100vh;
-
   display: grid;
   grid-template-rows: 38px minmax(0, 1fr);
 `;
